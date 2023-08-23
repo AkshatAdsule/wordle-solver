ed223eeb033bbb94dc56ab0b1b09a4397fec0371
AkshatAdsule <AkshatAdsule@users.noreply.github.com>
2023-08-23 09:00:55 +0000

    update words list

--- words.js
+++ words.js
@@ -1,5 +1,4 @@
 const WORDS = [
-	"spice",
 	"verve",
 	"wordy",
 	"ocean",
@@ -26,4 +25,11 @@ const WORDS = [
 	"exert",
 	"angel",
 	"music",
+	"frank",
+	"close",
+	"snare",
+	"stone",
+	"brush",
+	"carol",
+	"right",
 ];
