2aeb029440a15f540f7bdb6179d83947207f6add
AkshatAdsule <AkshatAdsule@users.noreply.github.com>
2023-10-11 09:01:04 +0000

    update words list

--- words.js
+++ words.js
@@ -1,5 +1,4 @@
 const WORDS = [
-	"snail",
 	"skunk",
 	"knelt",
 	"uncle",
@@ -26,4 +25,11 @@ const WORDS = [
 	"ardor",
 	"mania",
 	"flare",
+	"trade",
+	"limit",
+	"ninja",
+	"glaze",
+	"leash",
+	"toddy",
+	"meant",
 ];
