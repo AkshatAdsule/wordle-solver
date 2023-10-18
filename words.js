8f56ae3fc8ee9cf1dd4c5e5f973dcc19d6cacb37
AkshatAdsule <AkshatAdsule@users.noreply.github.com>
2023-10-18 09:00:52 +0000

    update words list

--- words.js
+++ words.js
@@ -1,5 +1,4 @@
 const WORDS = [
-	"adult",
 	"mercy",
 	"splat",
 	"occur",
@@ -26,4 +25,11 @@ const WORDS = [
 	"leash",
 	"toddy",
 	"meant",
+	"green",
+	"sassy",
+	"sight",
+	"trust",
+	"tardy",
+	"think",
+	"queue",
 ];
