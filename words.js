8470cfdcad28360e9da00dd262d39f4d258985ae
AkshatAdsule <AkshatAdsule@users.noreply.github.com>
2024-01-26 09:00:53 +0000

    update words list

--- words.js
+++ words.js
@@ -1,5 +1,4 @@
 const WORDS = [
-	"block",
 	"aloof",
 	"snake",
 	"ember",
@@ -24,4 +23,11 @@ const WORDS = [
 	"stash",
 	"psalm",
 	"ridge",
+	"price",
+	"match",
+	"build",
+	"heavy",
+	"apart",
+	"piper",
+	"smith",
 ];
