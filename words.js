7944737da6b4dfee57d450d2e8a90b991b93c10a
AkshatAdsule <AkshatAdsule@users.noreply.github.com>
2024-03-06 09:00:49 +0000

    update words list

--- words.js
+++ words.js
@@ -1,5 +1,4 @@
 const WORDS = [
-	"hunch",
 	"teary",
 	"clone",
 	"early",
@@ -26,4 +25,11 @@ const WORDS = [
 	"realm",
 	"force",
 	"taboo",
+	"frond",
+	"serum",
+	"plait",
+	"climb",
+	"wrist",
+	"finch",
+	"LASER",
 ];
