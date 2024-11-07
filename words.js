95cf8a04822cb91340f1d93cd61da9e5cf48b8a0
AkshatAdsule <AkshatAdsule@users.noreply.github.com>
2024-11-07 09:07:24 +0000

    update words list

--- words.js
+++ words.js
@@ -1,5 +1,4 @@
 const WORDS = [
-	"truly",
 	"event",
 	"ready",
 	"swell",
@@ -25,4 +24,11 @@ const WORDS = [
 	"hippo",
 	"dogma",
 	"tally",
+	"guile",
+	"shaky",
+	"crypt",
+	"endow",
+	"shove",
+	"hilly",
+	"hyena",
 ];
